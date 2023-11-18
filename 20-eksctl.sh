63a42dd30aa5ac907b843b35d88a61808c2a8930
Neel Aundhia <neelaundhia@gmail.com>
2023-11-18 21:31:06 +0100

    Fix typos in 20-eksctl.sh

--- 20-eksctl.sh
+++ 20-eksctl.sh
@@ -2,7 +2,6 @@
 
 # Install EKS CTL
 #
-# for ARM systems, set ARCH to: `arm64`, `armv6` or `armv7`
 brew install weaveworks/tap/eksctl
 mkdir -p ~/.oh-my-zsh/completions/
 eksctl completion zsh >~/.oh-my-zsh/completions/_eksctl
