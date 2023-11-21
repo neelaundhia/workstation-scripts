ca74f85b84d88889081c5356eb4d2e31da3a602f
Neel Aundhia <neel.aundhia@uniwise.dk>
2023-11-21 08:51:59 +0100

    Add aliases to kubectl installation

--- 15-kubectl.sh
+++ 15-kubectl.sh
@@ -6,6 +6,7 @@ curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stabl
 chmod +x ./kubectl
 mkdir -p ~/.oh-my-zsh/completions
 ./kubectl completion zsh >~/.oh-my-zsh/completions/_kubectl
+cp config/zsh/.zshrc.d/kubectl.source ~/.zshrc.d/kubectl.source
 mkdir -p ~/.local/bin
 mv ./kubectl ~/.local/bin/kubectl
 #
