8260b290d1253cbd0c61d98c8ec697e9d889f914
Neel Aundhia <neel.aundhia@uniwise.dk>
2023-12-05 11:03:37 +0100

    Add 06-sops.sh

---
+++ 06-sops.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Download the binary                                                                                                                                                      ─╯
+curl -LO https://github.com/getsops/sops/releases/download/v3.8.1/sops-v3.8.1.linux.amd64
+
+# Move the binary in to your PATH
+mv sops-v3.8.1.linux.amd64 /usr/local/bin/sops
+
+# Make the binary executable
+chmod +x /usr/local/bin/sops
