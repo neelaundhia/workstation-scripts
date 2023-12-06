1024714c0a253ec0cd96be8c56078957d75a9d97
Neel Aundhia <neelaundhia@gmail.com>
2023-12-06 21:05:10 +0100

    Update 06-sops.sh

--- 06-sops.sh
+++ 06-sops.sh
@@ -3,8 +3,12 @@
 # Download the binary                                                                                                                                                      ─╯
 curl -LO https://github.com/getsops/sops/releases/download/v3.8.1/sops-v3.8.1.linux.amd64
 
+# Preparation
+mkdir -p ~/.local/bin
+source ~/.profile
+
 # Move the binary in to your PATH
-mv sops-v3.8.1.linux.amd64 /usr/local/bin/sops
+mv sops-v3.8.1.linux.amd64 ~/.local/bin/sops
 
 # Make the binary executable
-chmod +x /usr/local/bin/sops
+chmod +x ~/.local/bin/sops
