c9c0012bd5f1933f1af348ea6385650f6593cf68
Neel Aundhia <neel.aundhia@uniwise.dk>
2023-12-19 16:16:43 +0100

    Update 05-cloudflare-warp.sh

--- 05-cloudflare-warp.sh
+++ 05-cloudflare-warp.sh
@@ -9,6 +9,6 @@ curl https://pkg.cloudflareclient.com/pubkey.gpg | sudo gpg --yes --dearmor --ou
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/cloudflare-client.list
 #
 # Install
-sudo apt-get update && sudo apt-get install cloudflare-warp
+sudo apt-get update && sudo apt-get install -y cloudflare-warp
 #
 # End
