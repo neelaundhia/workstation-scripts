295052507b43b54284a678ee61438b5c0cf82336
Neel Aundhia <neelaundhia@gmail.com>
2024-02-15 13:03:44 +0000

    Install sops with brew instead

--- 06-sops.sh
+++ 06-sops.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-# Download the binary                                                                                                                                                      ─╯
-curl -LO https://github.com/getsops/sops/releases/download/v3.8.1/sops-v3.8.1.linux.amd64
-
-# Preparation
-mkdir -p ~/.local/bin
-source ~/.profile
-
-# Move the binary in to your PATH
-mv sops-v3.8.1.linux.amd64 ~/.local/bin/sops
-
-# Make the binary executable
-chmod +x ~/.local/bin/sops
+# Install sops
+#
+brew install sops
+#
+# End
