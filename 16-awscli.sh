2559e1746d37b2488e61979539f6fb4aeee81df7
Neel Aundhia <neelaundhia@gmail.com>
2024-02-15 13:21:16 +0000

    Install awscli with brew instead

--- 16-awscli.sh
+++ 16-awscli.sh
@@ -2,10 +2,7 @@
 
 # Install AWS CLI
 #
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-rm -rf awscliv2.zip aws
+brew install awscli
 cp -f config/zsh/.zshrc.d/awscli.source ~/.zshrc.d/awscli.source
 #
 #
