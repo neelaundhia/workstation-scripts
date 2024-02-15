661413e7604c422a9f0b731e76626aa4b8a72fe4
Neel Aundhia <neelaundhia@gmail.com>
2024-02-15 13:14:59 +0000

    Install kubectl with brew instead

--- 15-kubectl.sh
+++ 15-kubectl.sh
@@ -2,13 +2,10 @@
 
 # Install kubectl
 #
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-chmod +x ./kubectl
+brew install kubernetes-cli
 mkdir -p ~/.oh-my-zsh/completions
-./kubectl completion zsh >~/.oh-my-zsh/completions/_kubectl
+kubectl completion zsh >~/.oh-my-zsh/completions/_kubectl
 cp config/zsh/.zshrc.d/kubectl.source ~/.zshrc.d/kubectl.source
-mkdir -p ~/.local/bin
-mv ./kubectl ~/.local/bin/kubectl
 #
 #
 # End
